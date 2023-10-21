923ea05d805e0e9893627098b47071abee4c98cc
Purnama Anaking <purnama.anaking@gmail.com>
2023-10-21 20:55:43 +0700

    Update

--- modul_05/nav-one/components/header.js
+++ modul_05/nav-one/components/header.js
@@ -1,27 +1,25 @@
-import { Box, Image, HStack, Heading } from "@gluestack-ui/themed";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import { Box, HStack, Image, Heading } from "native-base";
 import { TouchableOpacity } from "react-native";
-import { useNavigation } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Header = ({ title, withBack = false }) => {
   const trueGray900 = "#171717";
-  const navigation = useNavigation();
   return (
     <SafeAreaView>
       <StatusBar barStyle="light" backgroundColor={trueGray900} />
-      <Box bg="$trueGray900" p="$4">
+      <Box bg={"trueGray.900"} p={"4"}>
         <HStack justifyContent="space-between" alignItems="center">
           <HStack alignItems="center">
             {!withBack ? (
               <>
                 <Image
                   source={require("../assets/cnn.png")}
-                  w="$12"
-                  h="$12"
+                  w="12"
+                  h="12"
                   alt="CNN Logo"
-                  mr={"$3"}
+                  mr={"3"}
                 />
               </>
             ) : (
@@ -29,37 +27,37 @@ const Header = ({ title, withBack = false }) => {
                 activeOpacity={0.5}
                 onPress={() => navigation.goBack()}
               >
-                <Box mr={"$3"}>
+                <Box mr={"3"}>
                   <Ionicons name="arrow-back-outline" size={32} color="white" />
                 </Box>
               </TouchableOpacity>
             )}
-            <Heading color={"$white"}>{title}</Heading>
+            <Heading color={"white"}>{title}</Heading>
           </HStack>
 
           <HStack space={"2xl"}>
             <Image
               source={require("../assets/facebook.png")}
-              w="$5"
-              h="$5"
+              w="5"
+              h="5"
               alt="Facebook Icon"
             />
             <Image
               source={require("../assets/youtube.png")}
-              w="$5"
-              h="$5"
+              w="5"
+              h="5"
               alt="Facebook Icon"
             />
             <Image
               source={require("../assets/twitter.png")}
-              w="$5"
-              h="$5"
+              w="5"
+              h="5"
               alt="Facebook Icon"
             />
             <Image
               source={require("../assets/search.png")}
-              w="$5"
-              h="$5"
+              w="5"
+              h="5"
               alt="Search Icon"
             />
           </HStack>
---
+++ modul_05/nav-one/screens/news-detail.js
@@ -0,0 +1,30 @@
+import { Heading, ScrollView, Image, Box, Text, Divider } from "native-base";
+import Header from "../components/header";
+
+const NewsDetail = ({ route }) => {
+  // Get the params
+  const params = route.params.item;
+  return (
+    <>
+      <Header title={"News"} withBack="true" />
+      <ScrollView>
+        <Image
+          source={{ uri: params.image }}
+          w={"full"}
+          h={"48"}
+          alt="News Image"
+        />
+        <Box p={"4"}>
+          <Text mb={"1"}>{params.date}</Text>
+          <Heading lineHeight={"md"} fontSize={"2xl"}>
+            {params.title}
+          </Heading>
+          <Divider my={"4"} />
+          <Text fontSize={"md"}>{params.content}</Text>
+        </Box>
+      </ScrollView>
+    </>
+  );
+};
+
+export default NewsDetail;
